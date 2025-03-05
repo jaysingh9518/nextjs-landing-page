1e49ff9df15414c9588989632894c6ce478adbd0
Jay Prakash <jaysingh9518@gmail.com>
2025-03-05 15:05:24 +0530

    updated

--- src/components/ContactUs.tsx
+++ src/components/ContactUs.tsx
@@ -87,12 +87,12 @@ const ContactUs: React.FC = () => {
                             name="name" 
                             value={formData.name}
                             onChange={handleChange} 
-                            id="name" 
+                            id="namec" 
                             className="block pl-10 py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" 
                             placeholder=" " 
                             required 
                         />
-                        <label htmlFor="name" 
+                        <label htmlFor="namec" 
                             className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 left-10 origin-[0] peer-focus:left-10 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
                             Full Name
                         </label>
@@ -110,12 +110,12 @@ const ContactUs: React.FC = () => {
                             name="email" 
                             value={formData.email}
                             onChange={handleChange} 
-                            id="email" 
+                            id="emailc" 
                             className="block pl-10 py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" 
                             placeholder=" " 
                             required 
                         />
-                        <label htmlFor="email" 
+                        <label htmlFor="emailc" 
                             className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 left-10 origin-[0] peer-focus:left-10 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
                             Email Address
                         </label>
@@ -133,12 +133,12 @@ const ContactUs: React.FC = () => {
                             name="mobile"
                             value={formData.mobile}
                             onChange={handleChange}
-                            id="phone" 
+                            id="phonec" 
                             className="block pl-10 py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" 
                             placeholder=" " 
                             required 
                         />
-                        <label htmlFor="phone" 
+                        <label htmlFor="phonec" 
                             className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 left-10 origin-[0] peer-focus:left-10 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
                             Mobile
                         </label>
@@ -152,13 +152,13 @@ const ContactUs: React.FC = () => {
                             name="message"
                             value={formData.message}
                             onChange={handleChange}
-                            id="message" 
+                            id="messagec" 
                             className="block pl-10 py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" 
                             placeholder=" " 
                             rows={1}
                         >
                         </textarea>
-                        <label htmlFor="message" 
+                        <label htmlFor="messagec" 
                             className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 left-10 origin-[0] peer-focus:left-10 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
                             Any Question or Message
                         </label>
