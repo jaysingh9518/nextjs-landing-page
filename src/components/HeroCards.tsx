95efa12cb85d760c6a1e8ccd1c026ee5f551191e
Jay Prakash <jaysingh9518@gmail.com>
2025-03-22 10:59:42 +0530

    update

--- src/components/HeroCards.tsx
+++ src/components/HeroCards.tsx
@@ -656,7 +656,7 @@ const HeroCards = () => {
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                       <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
                     </svg>
-                    Get Exclusive Deals Now
+                    Get Exclusive Deals Now!
                   </button>
                 </div>
                 
